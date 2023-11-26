1fef3e4e15d6bdcc2c0054a10ab5dc82cbe23f08
jphillips <josh.phillips@fearnworks.com>
2023-11-26 15:05:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Fearnworks Custom Nodes 
+[!Fearnworks Banner](static/banner-1544x500.png)
+A collection of ComfyUI nodes. These nodes are tailored for specific tasks, such as counting files in directories and sorting text segments based on token counts.
+
+## Installation
+Clone this repository into your ComfyUI custom nodes directory.
+Ensure dependencies are installed, particularly the transformers and fnmatch libraries for Python. Use pip install transformers to install the required package.
+
+
+## Nodes:
+[!Token Count Nodes](static/TokenCount.png)
+### CountTokens
+This node counts the number of tokens in a given text string using the CLIP tokenizer from the transformers library. It's useful for analyzing text complexity and preprocessing text data.
+
+Inputs: CLIP model, Text (String)
+Outputs: Token Count (Integer)
+
+## TokenCountRanker
+Sorts segments and words within a text string by their token count, providing two lists: one for segments and one for words. This node aids in text analysis by highlighting the most complex parts of a given text.
+
+Inputs: CLIP model, Text (String)
+Outputs: Sorted Text by Token Count (String)
+
+## TrimToTokens
+Trims a text string to include only a specified number of tokens. This node is particularly useful in scenarios where you need to limit the length of text inputs to certain token thresholds.
+
+Inputs: CLIP model, Text (String), Number of Tokens (Integer)
+Outputs: Trimmed Text (String)
+
+## FileCountInDirectory
+Counts the number of files within a given directory, with an option to filter by file type. This node is essential for managing and analyzing file systems, ensuring efficient handling of directories and files.
+
+Inputs: Directory Path (String), File Types (String, optional)
+Outputs: File Count (Integer)
---
+++ fw_nodes.py
@@ -0,0 +1,188 @@
+import os 
+
+
+class CountTokens:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "clip": ("CLIP", {"forceInput": True}),
+                "text": ('STRING', {"forceInput": True}),
+            }
+        }
+
+    RETURN_TYPES = ('INT',)
+    FUNCTION = "count_tokens"
+    CATEGORY = "Fearnworks/Text/Tokens"
+
+    def count_tokens(self, clip, text):
+        print("Counting Tokens for text:", text)
+        tokens = clip.tokenize(text)
+
+        # Filter out padding tokens (token value is not 0) in the first list of 'g'
+        if 'g' in tokens and len(tokens['g']) > 0:
+            real_tokens = [token for token in tokens['g'][0] if token[0] != 0]
+            token_count = len(real_tokens)
+        else:
+            token_count = 0
+
+        print(f"Real token count: {token_count}")
+        cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
+        print("Contioned Shape:", cond)
+        print("############################")
+        print("Pooled Shape:", pooled)
+
+        return (token_count, )
+        
+    @classmethod
+    def IS_CHANGED(cls, **kwargs):
+        return float("NaN")
+
+class TrimToTokens:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "clip": ("CLIP", {"forceInput": True}),
+                "text": ('STRING', {"forceInput": True}),
+                "max_tokens": ('INT', {"forceInput": True})  # Maximum number of tokens allowed
+            }
+        }
+
+    RETURN_TYPES = ('STRING',)
+    FUNCTION = "trim_to_tokens"
+    CATEGORY = "Fearnworks/Text/Tokens"
+
+    def trim_to_tokens(self, clip, text, max_tokens):
+        print("Trimming text to fit within a specific number of tokens:", max_tokens)
+
+        # Split the text by commas
+        segments = text.split(',')
+        trimmed_segments = []
+
+        current_token_count = 0
+        for segment in segments:
+            tokens = clip.tokenize(segment)
+            if 'g' in tokens and len(tokens['g']) > 0:
+                real_tokens = [token for token in tokens['g'][0] if token[0] != 0]
+                segment_token_count = len(real_tokens)
+            else:
+                segment_token_count = 0
+
+            if current_token_count + segment_token_count <= max_tokens:
+                trimmed_segments.append(segment)
+                current_token_count += segment_token_count
+            else:
+                break  # Stop adding segments once the max token count is reached or exceeded
+
+        # Join the segments to form the trimmed text
+        trimmed_text = ','.join(trimmed_segments)
+
+        print(f"Trimmed text: {trimmed_text}")
+        return (trimmed_text, )
+
+    @classmethod
+    def IS_CHANGED(cls, **kwargs):
+        return float("NaN")
+    
+class TokenCountRanker:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "clip": ("CLIP", {"forceInput": True}),
+                "text": ('STRING', {"forceInput": True}),
+            }
+        }
+
+    RETURN_TYPES = ('STRING',)
+    FUNCTION = "sort_segments_and_words_by_token_count"
+    CATEGORY = "Fearnworks/Text/Tokens"
+
+    def sort_segments_and_words_by_token_count(self, clip, text):
+        print("Sorting segments and words by token count for text:", text)
+
+        # Split the text by commas and tokenize each segment
+        segments = text.split(',')
+        segment_token_info = []
+        word_token_info = []
+
+        for segment in segments:
+            words = segment.strip().split()
+            segment_total_token_count = 0
+
+            for word in words:
+                tokens = clip.tokenize(word)
+                if 'g' in tokens and len(tokens['g']) > 0:
+                    real_tokens = [token for token in tokens['g'][0] if token[0] != 0]
+                    word_token_count = len(real_tokens)
+                else:
+                    word_token_count = 0
+
+                segment_total_token_count += word_token_count
+                word_token_info.append((word, word_token_count))
+
+            segment_token_info.append((segment, segment_total_token_count))
+
+        # Sort segments and words by token count in descending order
+        sorted_segments = sorted(segment_token_info, key=lambda x: x[1], reverse=True)
+        sorted_words = sorted(word_token_info, key=lambda x: x[1], reverse=True)
+
+        # Construct strings with each sorted segment and word and their token counts
+        sorted_segments_text = '\n'.join(f"{segment} ({token_count} tokens)" for segment, token_count in sorted_segments)
+        sorted_words_text = '\n'.join(f"{word} ({token_count} tokens)" for word, token_count in sorted_words)
+
+        # Combine the two sorted lists into one string
+        combined_sorted_text = f"Sorted Segments:\n{sorted_segments_text}\n\nSorted Words:\n{sorted_words_text}"
+
+        print(combined_sorted_text)
+        return (combined_sorted_text, )
+
+    @classmethod
+    def IS_CHANGED(cls, **kwargs):
+        return float("NaN")
+
+import fnmatch
+class FileCountInDirectory:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "directory_path": ('STRING', {"forceInput": True}),
+                "file_types": ('STRING', {"forceInput": True, "default":"*"})  # String defining file types to count
+            }
+        }
+
+    RETURN_TYPES = ('INT',)
+    FUNCTION = "count_files_in_directory"
+    CATEGORY = "Fearnworks/File Operations"
+
+    def count_files_in_directory(self, directory_path, file_types):
+        print(f"Counting files in directory: {directory_path} with file types: {file_types}")
+
+        # Parse file types
+        file_types_list = file_types.split(',')
+
+        # Count the number of files matching the file types
+        try:
+            file_count = sum(1 for file in os.listdir(directory_path) 
+                             if any(fnmatch.fnmatch(file, pattern.strip()) for pattern in file_types_list) 
+                             and os.path.isfile(os.path.join(directory_path, file)))
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return (0, )  # Return 0 in case of an error
+
+        print(f"Number of files matching types {file_types}: {file_count}")
+        return (file_count, )
+
+    @classmethod
+    def IS_CHANGED(cls, **kwargs):
+        return float("NaN")
+
+NODE_CLASS_MAPPINGS = {
+    "Count Tokens (FW)": CountTokens,
+    "Trim To Tokens (FW)": TrimToTokens,
+    "Token Count Ranker(FW)": TokenCountRanker,
+    "Count Files in Directory (FW)": FileCountInDirectory
+
+}
